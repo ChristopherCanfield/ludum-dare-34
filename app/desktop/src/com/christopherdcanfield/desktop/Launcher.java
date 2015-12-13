74f9cff05dd31a723460b4d3f0fbb718fbdf969d
Christopher D Canfield <cdcanfield@gmail.com>
2015-12-13 14:24:22 -0500

    More Launcher layout updates

--- app/desktop/src/com/christopherdcanfield/desktop/Launcher.java
+++ app/desktop/src/com/christopherdcanfield/desktop/Launcher.java
@@ -24,11 +24,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Launcher extends Application
@@ -43,10 +49,10 @@ public class Launcher extends Application
 		launcherWindow = stage;
 		
 		root = new GridPane();
-		root.setGridLinesVisible(true);
+//		root.setGridLinesVisible(true);
 		root.setPadding(new Insets(15, 15, 0, 15));
 		root.setHgap(20);
-		root.setVgap(5);
+		root.setVgap(15);
 		
 		ColumnConstraints column1 = new ColumnConstraints();
 		column1.setPercentWidth(100);
@@ -58,11 +64,12 @@ public class Launcher extends Application
 		root.getRowConstraints().add(row1);
 		
 		RowConstraints row2 = new RowConstraints();
-		row2.setPercentHeight(50);
+		row2.setPercentHeight(57.5);
 		root.getRowConstraints().add(row2);
 		
 		// Title
 		Label titleLabel = new Label("xxxxx Game xxxxx");
+		titleLabel.setFont(Font.font(null, FontWeight.BOLD, 20));
 		GridPane.setHalignment(titleLabel, HPos.CENTER);
 		root.add(titleLabel, 0, 0);
 		
@@ -73,11 +80,12 @@ public class Launcher extends Application
 		
 		// Settings section
 		Label settingsLabel = new Label("Settings");
+		settingsLabel.setFont(Font.font(null, FontWeight.NORMAL, 18));
 		GridPane.setHalignment(settingsLabel, HPos.CENTER);
 		root.add(settingsLabel, 0, 2);
 		
 		GridPane settingsSection = new GridPane();
-		settingsSection.setGridLinesVisible(true);
+//		settingsSection.setGridLinesVisible(true);
 		settingsSection.setPadding(new Insets(0, 0, 0, 0));
 		settingsSection.setHgap(20);
 		settingsSection.setVgap(5);
@@ -85,26 +93,37 @@ public class Launcher extends Application
 		
 		// Display modes
 		Label displayModesLabel = new Label("Display Modes");
-		settingsSection.add(displayModesLabel, 0, 0);
+		settingsSection.add(displayModesLabel, 0, 2);
 
 		ObservableList<PrettifiedDisplayMode> options = getPrettifiedDisplayModes();
 		ComboBox<PrettifiedDisplayMode> displayModesComboBox = new ComboBox<>(options);
 		DisplayMode desktopDisplayMode = LwjglApplicationConfiguration.getDesktopDisplayMode();
 		displayModesComboBox.getSelectionModel().select(new PrettifiedDisplayMode(desktopDisplayMode));
-		settingsSection.add(displayModesComboBox, 1, 0);
+		settingsSection.add(displayModesComboBox, 1, 2);
 		
 		// Fullscreen checkbox
 		Label fullscreenLabel = new Label("Fullscreen");
-		settingsSection.add(fullscreenLabel, 0, 2);
+		settingsSection.add(fullscreenLabel, 0, 4);
 		
 		CheckBox fullscreenCheckbox = new CheckBox();
 		fullscreenCheckbox.setSelected(true);
-		settingsSection.add(fullscreenCheckbox, 1, 2);
+		settingsSection.add(fullscreenCheckbox, 1, 4);
+		
+		// Antialiasing checkbox
+		Label antialiasingLabel = new Label("Antialiasing");
+		settingsSection.add(antialiasingLabel, 0, 6);
+		
+		CheckBox antialiasingCheckbox = new CheckBox();
+		antialiasingCheckbox.setSelected(true);
+		settingsSection.add(antialiasingCheckbox, 1, 6);
 		
 		// Start game button
 		Button startGameButton = new Button("Start Game");
+		startGameButton.setTextFill(Color.SEAGREEN);
+		startGameButton.setFont(Font.font(null, FontWeight.BOLD, 14));
+//		startGameButton.setBackground(new Background(new BackgroundFill(Color.MEDIUMSEAGREEN, new CornerRadii(5), new Insets(0, 0, 0, 0))));
 		GridPane.setHalignment(startGameButton, HPos.CENTER);
-		root.add(startGameButton, 0, 6);
+		root.add(startGameButton, 0, 7);
 		startGameButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
@@ -120,6 +139,7 @@ public class Launcher extends Application
 					config.backgroundFPS = 30;
 					config.title = "Canfield LD34";
 					config.fullscreen = fullscreenCheckbox.isSelected();
+					config.samples = antialiasingCheckbox.isSelected() ? 8 : 0;
 					gameApp = new LwjglApplication(new GameApp(), config);
 				} catch (Exception e) {
 					stage.show();
@@ -131,6 +151,7 @@ public class Launcher extends Application
 		scene.setFill(Color.WHITE);
 
 		stage.setTitle("Canfield Ludum Dare 34");
+		stage.setResizable(false);
 		stage.centerOnScreen();
 		stage.setScene(scene);
 		stage.show();
